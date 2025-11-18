b110b86c69a48e4722f49976a088a0ca4c2bfbec
aissa-m <issamaymoutt17@gmail.com>
2025-11-18 23:47:27 +0100

    -

--- App.tsx
+++ App.tsx
@@ -1,32 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
-import ChatScreen from './src/screens/ChatScreen';
-import ProductDetailScreen from './src/screens/ProductDetailScreen';
-import MainTabs from './src/navigation/MainTabs';
-
-const Stack = createNativeStackNavigator();
+// App.tsx
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/hooks/useAuth";
+import AppNavigator from "./src/navigation/AppNavigator";
+import "./src/i18n"; 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Inscription' }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio' }} />
-        <Stack.Screen name="Chat" component={ChatScreen} options={{ title: 'Chat' }} />
-        <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{
-          title: 'Détails du produit', // 👈 cambia el título aquí
-        }} />
-        {/* Área principal tras login */}
-        <Stack.Screen
-          name="MainTabs"
-          component={MainTabs}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
